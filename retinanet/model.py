768e3f204d4571263f6826d9d088d32f75e63e53
woutv <woutvanlierop@hotmail.com>
2022-05-03 10:02:35 +0200

    dropout added

--- retinanet/model.py
+++ retinanet/model.py
@@ -81,6 +81,8 @@ class RegressionModel(nn.Module):
         self.conv4 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
         self.act4 = nn.ReLU()
 
+        self.dropout = nn.Dropout(0.5)
+
         self.output = nn.Conv2d(feature_size, num_anchors * 4, kernel_size=3, padding=1)
 
     def forward(self, x):
@@ -89,6 +91,7 @@ class RegressionModel(nn.Module):
 
         out = self.conv2(out)
         out = self.act2(out)
+        out = self.dropout(out)
 
         out = self.conv3(out)
         out = self.act3(out)
@@ -116,6 +119,7 @@ class ClassificationModel(nn.Module):
 
         self.conv2 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
         self.act2 = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
 
         self.conv3 = nn.Conv2d(feature_size, feature_size, kernel_size=3, padding=1)
         self.act3 = nn.ReLU()
@@ -126,12 +130,14 @@ class ClassificationModel(nn.Module):
         self.output = nn.Conv2d(feature_size, num_anchors * num_classes, kernel_size=3, padding=1)
         self.output_act = nn.Sigmoid()
 
+
     def forward(self, x):
         out = self.conv1(x)
         out = self.act1(out)
 
         out = self.conv2(out)
         out = self.act2(out)
+        out = self.dropout(out)
 
         out = self.conv3(out)
         out = self.act3(out)
